39d3036d9b35ace80ca9de6ef063a3b806a1749e
Yull Timoteo <yull.timoteo.z@uni.pe>
2024-05-09 07:37:41 -0500

    initial creation of object p

---
+++ src/elements/element-html.js
@@ -0,0 +1,8 @@
+class ElementHtml {
+  constructor(id, className, classBootstrap) {
+    this.id = id || "";
+    this.className = className || "";
+    this.classBootstrap = classBootstrap || "";
+  }
+}
+export { ElementHtml };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { ElementP } from "./elements/element-p.js";
+
 function app() {
   const root = document.getElementById("root");
   // Crear un elemento <p> en el DOM
@@ -8,9 +10,14 @@ function app() {
   paragraph.classList.add();
   paragraph.id = 223;
   paragraph.textContent = "Este es el contenido del párrafo."; // Puedes cambiar este texto por lo que desees
+  console.log(paragraph.outerHTML);
 
   // Añadir el elemento <p> al elemento con id "root" en el DOM
   root.appendChild(paragraph);
+
+  const paragraph2 = new ElementP(1, "xxx", "xxx", "Hola Mundo");
+  console.log(paragraph2);
+  console.log(paragraph2.element.outerHTML);
 }
 
 app();
