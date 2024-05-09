fbc866039dc30cf5fc79dc4bdfe598a60d0cfb1b
Yull Timoteo <yull.timoteo.z@uni.pe>
2024-05-09 07:12:08 -0500

    update files

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
-const main = document.getElementById("root");
-main.textContent = "Hola";
+function app() {
+  const root = document.getElementById("root");
+  // Crear un elemento <p> en el DOM
+  const paragraph = document.createElement("p");
+
+  // Configurar las propiedades del elemento <p> con los valores de data
+  paragraph.className = "hola";
+  paragraph.classList.add();
+  paragraph.id = 223;
+  paragraph.textContent = "Este es el contenido del párrafo."; // Puedes cambiar este texto por lo que desees
+
+  // Añadir el elemento <p> al elemento con id "root" en el DOM
+  root.appendChild(paragraph);
+}
+
+app();
